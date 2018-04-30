ea7ce1148a19d984b0396e53bada42ed0ba18150
Arun Wilson <cyberinfoscripter@gmail.com>
2018-04-30 09:51:19 +0530

    Modified server.js acc to eslint reccomendations;

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-//create express app
+// create express app
 const app = express();
 
-//parse requests of content-type - application/x-www-formurlencoded
-app.use(bodyParser.urlencoded({extended:true}));
+// parse requests of content-type - application/x-www-formurlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
 
 // define a dirst & simple route
-
-app.get('/',(req,res)=>{
-    res.json({"message":"Welcome to cybernfoscriipter application."});
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to cybernfoscriipter application.' });
 });
 
 
-app.listen(3000,()=>{
-
-    console.log('App listening in port 3000');
-
+app.listen(3000, () => {
+  console.log('App listening in port 3000');
 });
