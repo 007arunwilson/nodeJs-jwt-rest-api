d8ad2b59aa19f10c3a819dbb4518347c01eb40f8
Arun Wilson <cyberinfoscripter@gmail.com>
2018-04-30 10:00:04 +0530

    Added new Console Object my extending console.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const Console = {
+  new: console,
+  protottype: console,
+};
+
 // create express app
 const app = express();
 
@@ -13,10 +18,10 @@ app.use(bodyParser.json());
 
 // define a dirst & simple route
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to cybernfoscriipter application.' });
+  res.json({ message: 'Welcome to cyberinfoscripter application.' });
 });
 
 
 app.listen(3000, () => {
-  console.log('App listening in port 3000');
+  Console.new.log('App listening in port 3000');
 });
