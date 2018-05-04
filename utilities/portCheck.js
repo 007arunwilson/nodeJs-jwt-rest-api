658c458d251351480b45f6066d59b299fbe5cb2c
Arun Wilson <cyberinfoscripter@gmail.com>
2018-05-04 19:27:19 +0530

    Cleanauped portCheck .js

--- utilities/portCheck.js
+++ utilities/portCheck.js
@@ -15,31 +15,3 @@ const portCheck = portNumber => (
 );
 
 module.exports = portCheck;
-
-// const net = require('net');
-
-// const server = net.createServer();
-
-// server.once('listening', () => {
-// // close the server if listening doesn't fail
-//   server.close();
-// });
-
-// server.once('error', err => console.log('error on listening', err));
-
-// server.listen('3000');
-
-// var server = net.createServer();
-
-// server.once('error', function(err) {
-//   if (err.code === 'EADDRINUSE') {
-//     // port is currently in use
-//   }
-// });
-
-// server.once('listening', function() {
-//   // close the server if listening doesn't fail
-//   server.close();
-// });
-
-// server.listen(/* put the port to check here */);
